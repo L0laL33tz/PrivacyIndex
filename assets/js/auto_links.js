5813a13e6b223e8d0a246deccb445da473b7b02b
SKP <skp@skp.rocks>
2024-01-21 20:33:49 +0100

    using jekyll to render the pages

---
+++ assets/js/auto_links.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+  const linkerFunction = function(type) {
+    // Select all anchor tags with data-type="type"
+    const typeAnchors = document.querySelectorAll(`a[data-type="${type}"]`);
+
+    // Process each anchor tag
+    typeAnchors.forEach(function(anchor) {
+      const typeText = anchor.textContent;
+      // double encode cause the path actually contains the url encoded text
+      const escapedTypeText = encodeURIComponent(encodeURIComponent(typeText).toLowerCase());
+
+      // Construct the href attribute based on the escaped type
+      anchor.href = `/${type}/` + escapedTypeText;
+
+      // Add the 'typeText' class to the anchor
+      anchor.className = type;
+      anchor.addEventListener("click", function(event) {
+        // do not bubble the click event up to the parent table row
+        event.stopPropagation();
+      });
+    });
+  };
+
+  linkerFunction("tag");
+  linkerFunction("glossary");
+});
